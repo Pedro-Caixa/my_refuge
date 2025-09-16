3dc98022c6ca0afb98a75c035502c17a870a6e17
MarcosVTavares <marquinhotavares03@gmail.com>
2025-09-16 19:23:57 -0300

    feat: tela de loguin sendo feita adiciona models feito por enquanto

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
 
-void main() {
-  runApp(MyRefugeApp());
-}
-
-class MyRefugeApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Refuge',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
-      home: WelcomeScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
